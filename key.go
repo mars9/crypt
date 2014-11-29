78de7f161ef648704f87f09a8a7af8b4e2e1c83e
Markus Sonderegger <markuss@fzvnbx10.lan>
2014-11-29 14:42:40 +0100

    1417268560

--- key.go
+++ key.go
@@ -3,8 +3,8 @@ package crypt
 import (
 	"crypto/sha1"
 
-	"code.google.com/p/go.crypto/pbkdf2"
-	"code.google.com/p/go.crypto/scrypt"
+	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/scrypt"
 )
 
 type Key interface {
